ac1a328ef60f8fd93aea1305055e92ab869101db
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-05-29 04:55:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,16 @@ lassie.name ="Lassie"
 
 lassie.name #=> "lassie"
 
+
 end
+
+class Dog
+  def name=(dogs_names)
+    @this_dogs_name = dogs_names
+    
+  end
+  
+  def name 
+    @this_dogs_name
+    
+    
